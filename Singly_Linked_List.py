f9cbc79899856d19b1b3990e3ea75a52847265f0
PHD24# <pranavhdev@gmail.com>
2021-01-07 00:47:25 -0500

    Update Singly_Linked_List.py

--- Singly_Linked_List.py
+++ Singly_Linked_List.py
@@ -1,65 +1,144 @@
+# Code: Singly Linked-List using Python
+# Date: 01/06/2021
+# Author: Pranav H. Deo
+
+# Class Node
 class Node:
-  def __init__(self, data, nextNode=None):
-    self.data = data
-    self.nextNode = None
+    def __init__(self, Data=None, Next=None):
+        self.Data = Data
+        self.Next = None
 
-choice = 1
-dt = int(input("Enter the value for the root node : "))
-Head = Node(dt)
-Head.nextNode = None
 
-while(choice!=0):
+print('****** Singly Linked-List ******\n')
+# Creating the Head Node:
+head = Node()
+p = head
+q = head
 
-  choice = int(input("\n\n####Enter operation : 0.Exit ; 1.Append Node; 2.Add Node at Pos ; 3.Delete Node ; 4.Display LinkedList : "))
-  
-  if(choice==0):
-    break;
+while 1:
+    print('\n> Operations : 0. Create Linked-List ; 1. Append Data ; 2. Add Data to Location ; '
+          '3. Search Data ; 4. Delete Data ; 5. Display Linked-List ; 6. Exit')
+    choice = int(input('# Enter Choice : '))
 
-  elif(choice==1):
-    p = Head
-    while(p.nextNode!=None):
-      p = p.nextNode
-    num = int(input(">Enter the number of nodes to add : "))
-    while(num!=0):
-      dt = int(input(">Enter the value for the node : "))
-      q = Node(dt)
-      q.nextNode = None
-      p.nextNode = q
-      p = q
-      num-=1
+    if choice == 0:
+        print('\n----- Choice : Create Linked-List -----')
+        n = int(input('> Enter Nodes : '))
+        # Creating the Nodes for the Linked-List:
+        for i in range(0, n):
+            if i == 0:
+                dt = int(input('* Enter Data for Head Node : '))
+                head.Data = dt
+                head.Next = None
+            else:
+                dt = int(input('* Enter Data for Node ' + str(i + 1) + ' : '))
+                new_Node = Node(dt, None)
+                p.Next = new_Node
+                p = new_Node
 
-  elif(choice==2):
-    pos = int(input(">Enter the Position : "))
-    dt = int(input(">Enter the value of the Node : "))
-    p = Head
-    count = 0
-    while(count!=pos-1):
-      p = p.nextNode
-      count+=1
-    q = Node(dt)
-    q.nextNode = p.nextNode
-    p.nextNode = q
+    # Appending Data to the Linked-List
+    elif choice == 1:
+        print('\n----- Choice : Append Data -----')
+        dt = int(input('* Enter Data : '))
+        addn_node = Node(dt, None)
+        p = head
+        while p.Next is not None:
+            p = p.Next
+        p.Next = addn_node
 
-  elif(choice==3):
-    flag=0
-    dt = int(input(">Enter the Data to Delete : "))
-    p = Head
-    q = p.nextNode
-    while(q.data!=dt):
-      q = q.nextNode
-      if(q.nextNode==None):
-        flag=1
-        break
-      p = p.nextNode
-    if(flag==0):
-      p.nextNode = q.nextNode
-      q.nextNode = None
+    # Adding Data to a given Location
+    elif choice == 2:
+        print('\n----- Choice : Add Data to Location -----')
+        dt = int(input('* Enter Data : '))
+        loc = int(input('* Enter Location : '))
+        addn_node = Node(dt, None)
+        p = head
+        q = head
+        flag = 0
+        count = 0
+        while q is not None:
+            if loc == count and loc == 0:
+                addn_node.Next = head
+                head = addn_node
+                p = head
+                q = head
+                flag = 1
+                print('# ', dt, ' Inserted..')
+                break
+            elif loc == count and loc != 0:
+                while p.Next != q:
+                    p = p.Next
+                addn_node.Next = q
+                p.Next = addn_node
+                p = head
+                q = head
+                flag = 1
+                print('# ', dt, ' Inserted..')
+                break
+            else:
+                count += 1
+                q = q.Next
+        if q is None and loc == count and flag == 0:
+            while p.Next is not None:
+                p = p.Next
+            p.Next = addn_node
+            p = head
+            q = head
+            flag = 1
+            print('# ', dt, ' Inserted..')
+        if flag == 0:
+            print('# ', dt, ' Cannot be Inserted...')
+
+    # Searching Data inside the Linked-List
+    elif choice == 3:
+        print('\n----- Choice : Search Data -----')
+        dt = int(input('* Enter Data : '))
+        p = head
+        flag = 0
+        while p is not None:
+            if p.Data == dt:
+                print('# ', dt, ' Found..')
+                flag = 1
+                break
+            else:
+                p = p.Next
+        if flag == 0:
+            print('# ', dt, ' Not Found..')
+
+    # Delete Data from the Linked-List
+    elif choice == 4:
+        print('\n----- Choice : Delete Data -----')
+        dt = int(input('* Enter Data : '))
+        p = head
+        q = p.Next
+        flag = 0
+        while q is not None:
+            if dt == head.Data:
+                head = q
+                p = head
+                q = p.Next
+                flag += 1
+                print('# ', dt, ' Found and Deleted..')
+            if q.Data == dt:
+                while p.Next != q:
+                    p = p.Next
+                p.Next = q.Next
+                q.Next = None
+                flag += 1
+                print('# ', dt, ' Found and Deleted..')
+                q = p.Next
+            else:
+                q = q.Next
+        if flag == 0:
+            print('# ', dt, ' Not Found and Not Deleted..')
+
+    # Displaying the Linked-List
+    elif choice == 5:
+        print('\n----- Choice : Display Linked-List -----')
+        p = head
+        while p is not None:
+            print(p.Data, end='->')
+            p = p.Next
+        print('\n')
+
     else:
-      print("Data Not Present")
-    
-
-  elif(choice==4):
-    p = Head
-    while(p!=None):
-      print(p.data,",", end='')
-      p = p.nextNode
+        break
